cc9079c474b941237796473ac4661b1102446a79
miao.cui <miao.cui@bianlifeng.com>
2019-03-13 00:29:01 +0800

    add

--- src/com/bycuimiao/simple/SortBox.java
+++ src/com/bycuimiao/simple/SortBox.java
@@ -7,4 +7,37 @@ package com.bycuimiao.simple;
  * @since 12 March 2019
  */
 public class SortBox {
+	public static void main(String[] args) {
+		int [] arr = {2,3,1,4,5,8,7,9,6};
+		//bubblingSort(arr);
+		quicklySort(arr,0,arr.length-1);
+		for(int i = 0 ; i < arr.length ; i ++){
+			System.out.println(arr[i]);
+		}
+	}
+
+	/**
+	 * 冒泡排序
+	 * @param arr
+	 */
+	private static void bubblingSort(int [] arr){
+		int t;
+		for(int i = 0 ; i < arr.length ; i ++){
+			for (int j = i + 1 ; j < arr.length ; j ++){
+				if(arr[i] > arr[j]){
+					t = arr[i];
+					arr[i] = arr[j];
+					arr[j] = t;
+				}
+			}
+		}
+	}
+
+	/**
+	 * 快速排序
+	 * @param
+	 */
+	public static void quicklySort(int[] a,int low,int high){
+
+	}
 }
