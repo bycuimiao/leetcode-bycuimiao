42534e5d0c2eafd13705fda54919bea6719f6ccb
bycuimiao <373013128@qq.com>
2019-11-05 21:03:37 +0800

    WZH

---
+++ src/com/wzh/leetcode/Hello.java
@@ -0,0 +1,15 @@
+package com.wzh.leetcode;
+
+/**
+ * @Description TODO
+ * @Date 2019/11/5 9:01 下午
+ * @Author bycuimiao
+ **/
+public class Hello {
+    /**
+     * 不积跬步无以至千里,不积小流无以成江海；
+     * 骐骥一跃，不能十步；驽马十驾，功在不舍；
+     * 锲而舍之朽木不折，锲而不舍金石可镂
+     */
+
+}
