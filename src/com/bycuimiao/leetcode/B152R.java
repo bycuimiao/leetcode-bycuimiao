d963300f64ceb083a80d586da4a8119ce4e47788
miao.cui <miao.cui@quantgroup.cn>
2019-11-19 19:00:34 +0800

    commit

---
+++ src/com/bycuimiao/leetcode/B152R.java
@@ -0,0 +1,63 @@
+package com.bycuimiao.leetcode;
+
+/**
+ * @author miao.cui
+ * @description:
+ * 给定一个整数数组 nums ，找出一个序列中乘积最大的连续子序列（该序列至少包含一个数）。
+ *
+ * 示例 1:
+ *
+ * 输入: [2,3,-2,4]
+ * 输出: 6
+ * 解释: 子数组 [2,3] 有最大乘积 6。
+ * 示例 2:
+ *
+ * 输入: [-2,0,-1]
+ * 输出: 0
+ * 解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-product-subarray
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2019-11-19 17:41
+ */
+public class B152R {
+    /**
+     * 标签：动态规划
+     * 遍历数组时计算当前最大值，不断更新
+     * 令imax为当前最大值，则当前最大值为 imax = max(imax * nums[i], nums[i])
+     * 由于存在负数，那么会导致最大的变最小的，最小的变最大的。因此还需要维护当前最小值imin，imin = min(imin * nums[i], nums[i])
+     * 当负数出现时则imax与imin进行交换再进行下一步计算
+     *
+     * 作者：guanpengchn
+     * 链接：https://leetcode-cn.com/problems/maximum-product-subarray/solution/hua-jie-suan-fa-152-cheng-ji-zui-da-zi-xu-lie-by-g/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param nums
+     * @return
+     */
+    public int maxProduct(int[] nums) {
+        //imax表示以当前节点为终结节点的最大连续子序列乘积
+        //imin表示以当前节点为终结节点的最小连续子序列乘积
+        int max = Integer.MIN_VALUE, imax = 1, imin = 1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] < 0){
+                int tmp = imax;
+                imax = imin;
+                imin = tmp;
+            }
+            imax = Math.max(imax*nums[i], nums[i]);
+            imin = Math.min(imin*nums[i], nums[i]);
+
+            max = Math.max(max, imax);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1,2,-2,2,-2};
+        B152R obj = new B152R();
+        int max = obj.maxProduct(nums);
+        System.out.println(max);
+    }
+}
