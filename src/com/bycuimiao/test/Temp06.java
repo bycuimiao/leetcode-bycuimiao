d3a448b4d5bb0ff69ace061854493ffe09332836
bycuimiao <373013128@qq.com>
2019-12-04 23:51:33 +0800

    commit

---
+++ src/com/bycuimiao/test/Temp06.java
@@ -0,0 +1,23 @@
+package com.bycuimiao.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description TODO
+ * @Date 2019/12/4 10:46 下午
+ * @Author bycuimiao
+ **/
+public class Temp06 {
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE + 1);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
+        System.out.println(Integer.toBinaryString(Integer.MIN_VALUE));
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+    }
+}
