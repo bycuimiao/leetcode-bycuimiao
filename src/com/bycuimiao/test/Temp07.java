98366658123ae661cb8c1e521c3a174d84ebbc6a
bycuimiao <373013128@qq.com>
2019-12-10 22:51:42 +0800

    commit

---
+++ src/com/bycuimiao/test/Temp07.java
@@ -0,0 +1,14 @@
+package com.bycuimiao.test;
+
+import java.util.HashMap;
+
+/**
+ * @Description TODO
+ * @Date 2019/12/10 10:19 下午
+ * @Author bycuimiao
+ **/
+public class Temp07 {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+    }
+}
