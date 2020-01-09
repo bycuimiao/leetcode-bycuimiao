d5e33470906e0ab4e24c175b935a53848f8c6eeb
bycuimiao <373013128@qq.com>
2020-01-09 21:28:53 +0800

    commit

---
+++ src/com/bycuimiao/leetcode/A112.java
@@ -0,0 +1,51 @@
+package com.bycuimiao.leetcode;
+
+/**
+ * @Description
+ * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例: 
+ * 给定如下二叉树，以及目标和 sum = 22，
+ *
+ *               5
+ *              / \
+ *             4   8
+ *            /   / \
+ *           11  13  4
+ *          /  \      \
+ *         7    2      1
+ * 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/path-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Date 2020/1/9 9:11 下午
+ * @Author bycuimiao
+ **/
+public class A112 {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null)
+            return false;
+
+        sum -= root.val;
+        if ((root.left == null) && (root.right == null))
+            return (sum == 0);
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+    }
+
+    public static void main(String[] args) {
+        A112 obj = new A112();
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        //root.left.right = new TreeNode(null);
+        root.right = new TreeNode(8);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        System.out.println(obj.hasPathSum(root,22));
+    }
+}
